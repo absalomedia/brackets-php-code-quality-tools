2c52002970b0c13e0988e7dea0349caf1f57ae79
Lawrence <media@absalom.biz>
2016-11-17 11:37:48 +1000

    Closure linting

--- main.js
+++ main.js
@@ -5,52 +5,52 @@
  * @author Mikael Jorhult
  * @license http://mikaeljorhult.mit-license.org MIT
  */
-define( function( require, exports, module ) {
+define(function(require, exports, module ) {
 	'use strict';
-	
+
 	// Get module dependencies.
-	var CommandManager = brackets.getModule( 'command/CommandManager' ),
-		ExtensionUtils = brackets.getModule( 'utils/ExtensionUtils' ),
-		Menus = brackets.getModule( 'command/Menus' ),
-		PreferencesManager = brackets.getModule( 'preferences/PreferencesManager' ),
-        Mustache = brackets.getModule("thirdparty/mustache/mustache"),
-		
+	var CommandManager = brackets.getModule('command/CommandManager'),
+		ExtensionUtils = brackets.getModule('utils/ExtensionUtils'),
+		Menus = brackets.getModule('command/Menus'),
+		PreferencesManager = brackets.getModule('preferences/PreferencesManager'),
+      Mustache = brackets.getModule('thirdparty/mustache/mustache'),
+
 		// Extension Modules.
-		Defaults = require( 'modules/Defaults' ),
-		ParserManager = require( 'modules/ParserManager' ), // jshint ignore:line
-		SettingsDialog = require( 'modules/SettingsDialog' ),
-		Strings = require( 'modules/Strings' ),
-		
+		Defaults = require('modules/Defaults'),
+		ParserManager = require('modules/ParserManager'), // jshint ignore:line
+		SettingsDialog = require('modules/SettingsDialog'),
+		Strings = require('modules/Strings'),
+
 		// Setup extension.
 		COMMAND_ID_SETTINGS = 'mikaeljorhult.bracketsPHPLintTools.settings',
-		preferences = PreferencesManager.getExtensionPrefs( 'mikaeljorhult.bracketsPHPLintTools' ),
-		
+		preferences = PreferencesManager.getExtensionPrefs('mikaeljorhult.bracketsPHPLintTools'),
+
 		// Hook into menus.
-		menu = Menus.getMenu( Menus.AppMenuBar.VIEW_MENU );
-	
+		menu = Menus.getMenu(Menus.AppMenuBar.VIEW_MENU);
+
 	// Define preferences.
-	preferences.definePreference( 'php-available', 'boolean', false );
-	preferences.definePreference( 'enabled-tools', 'array', Defaults.enabledTools );
-	preferences.definePreference( 'phpcs-standards', 'array', Defaults.phpcsStandards );
-	preferences.definePreference( 'phpmd-rulesets', 'array', Defaults.phpmdRulesets );
-	preferences.definePreference( 'php7cc-opts', 'array', Defaults.php7ccOpts );	
-    preferences.definePreference( 'phpcsfixer-level', 'array', Defaults.phpcsfixerLevel );
-	preferences.definePreference( 'php-location', 'string', Defaults.PHPLocation );
-	
+	preferences.definePreference('php-available', 'boolean', false);
+	preferences.definePreference('enabled-tools', 'array', Defaults.enabledTools);
+	preferences.definePreference('phpcs-standards', 'array', Defaults.phpcsStandards);
+	preferences.definePreference('phpmd-rulesets', 'array', Defaults.phpmdRulesets);
+	preferences.definePreference('php7cc-opts', 'array', Defaults.php7ccOpts);
+  preferences.definePreference('phpcsfixer-level', 'array', Defaults.phpcsfixerLevel);
+	preferences.definePreference('php-location', 'string', Defaults.PHPLocation);
+
 	// Register extension.
-	CommandManager.register( Strings.EXTENSION_NAME, COMMAND_ID_SETTINGS, showSettingsDialog );
-	
+	CommandManager.register(Strings.EXTENSION_NAME, COMMAND_ID_SETTINGS, showSettingsDialog);
+
 	// Add command to menu.
-	if ( menu !== undefined ) {
+	if (menu !== undefined) {
 		menu.addMenuDivider();
-		menu.addMenuItem( COMMAND_ID_SETTINGS );
+		menu.addMenuItem(COMMAND_ID_SETTINGS);
 	}
-	
+
 	// Load stylesheet.
-	ExtensionUtils.loadStyleSheet( module, 'php-tools.css' );
-	
+	ExtensionUtils.loadStyleSheet(module, 'php-tools.css');
+
 	// Show settings dialog.
 	function showSettingsDialog() {
-		SettingsDialog.show( preferences );
+		SettingsDialog.show(preferences);
 	}
-} );
+});
--- modules/SettingsDialog.js
+++ modules/SettingsDialog.js
@@ -1,214 +1,172 @@
-define( function( require, exports ) {
-	'use strict';
-	
-	// Get module dependencies.
-	var Dialogs = brackets.getModule( 'widgets/Dialogs' ),
-		
-		// Extension Modules.
-		Defaults = require( 'modules/Defaults' ),
-		Strings = require( 'modules/Strings' ),
-		ParserManager = require( 'modules/ParserManager' ),
-		
-		// Templates.
-		settingsDialogTemplate = require( 'text!../html/settings-dialog.html' ),
-		
-		// Variables.
-		dialog,
-		$dialog,
-		preferences;
-	
-	/**
-	 * Reset all preferences to defaults.
-	 */
-	function resetValues() {
-		setValues( Defaults );
-		
-		preferences.save();
-	}
-	
-	/**
-	 * Retrieve all values from settings dialog.
-	 */
-	function getValues() {
-		var phpLocation = $dialog.find( 'input[ name="php_location" ]' ).val();
-		
-		var values = {
-			PHPLocation: ParserManager.sanitizePHPLocation( phpLocation ),
-			enabledTools: getCheckboxArray( 'enabled' ),
-			phpcsStandards: getCheckboxArray( 'phpcs-standards' ),
-			php7ccOpts: getCheckboxArray( 'php7cc-opts' ),
-			phpcsfixerLevel: getCheckboxArray( 'phpcsfixer-level' ),
-			phpmdRulesets: getCheckboxArray( 'phpmd-rulesets' )
-		};
-		
-		return values;
-	}
-	
-	/**
-	 * Get values of checked checkboxes with same name.
-	 */
-	function getCheckboxArray( name ) {
-		// Return values of checked checkboxes.
-		return $( 'input[ name="' + name + '[]" ]:checked', $dialog ).map( function() {
-			return this.value;
-		} ).get();
-	}
+define(function(require, exports) {
+  'use strict';
+  // Get module dependencies.
+  var Dialogs = brackets.getModule('widgets/Dialogs') ,
+      // Extension Modules.
+      Defaults = require('modules/Defaults') ,
+      Strings = require('modules/Strings') ,
+      ParserManager = require('modules/ParserManager') ,
+      // Templates.
+      settingsDialogTemplate = require('text!../html/settings-dialog.html') ,
+      // Variables.
+      dialog,
+      $dialog,
+      preferences;
+  /**
+     * Reset all preferences to defaults.
+     */
+  function resetValues() {
+        setValues(Defaults);
+        preferences.save();
+  }
+  /**
+     * Retrieve all values from settings dialog.
+     */
+  function getValues() {
+        var phpLocation = $dialog.find('input[ name="php_location" ]').val();
+        var values = {
+      PHPLocation: ParserManager.sanitizePHPLocation(phpLocation) ,
+          enabledTools: getCheckboxArray('enabled') ,
+          phpcsStandards: getCheckboxArray('phpcs-standards') ,
+          php7ccOpts: getCheckboxArray('php7cc-opts') ,
+          phpcsfixerLevel: getCheckboxArray('phpcsfixer-level') ,
+          phpmdRulesets: getCheckboxArray('phpmd-rulesets')
+        };
+        return values;
+  }
+  /**
+     * Get values of checked checkboxes with same name.
+     */
+  function getCheckboxArray(name) {
+        // Return values of checked checkboxes.
+        return $('input[ name="' + name + '[]" ]:checked', $dialog).map(function() {
+      return this.value;
+        }).get();
+  }
+  /**
+     * Set each value of the preferences in dialog.
+     */
+  function setValues(values) {
+        setCheckboxesFromArray('enabled', values.enabledTools);
+        setCheckboxesFromArray('phpcs-standards', values.phpcsStandards);
+        setCheckboxesFromArray('phpcsfixer-level', values.phpcsfixerLevel);
+        setCheckboxesFromArray('php7cc-opts', values.php7ccOpts);
+        setCheckboxesFromArray('phpmd-rulesets', values.phpmdRulesets);
+        $dialog.find('input[ name="php_location" ]').val(values.PHPLocation);
+  }
+  /**
+     * Check checkboxes according to valuess in array.
+     */
+  function setCheckboxesFromArray(name, valueArray) {
+        // Walk through each checkbox in dialog with supplied name.
+        $dialog.find('input[ name="' + name + '[]" ]').each(function(index, element) {
+      var $this = $(element);
+      // Make checkbox checked if its value is in array.
+      $this.prop('checked', valueArray.indexOf($this.prop('value')) !== -1);
+        });
+  }
+  /**
+     * Initialize dialog values.
+     */
+  function init() {
+        var values = {
+      enabledTools: preferences.get('enabled-tools') ,
+          phpcsStandards: preferences.get('phpcs-standards') ,
+          phpcsfixerLevel: preferences.get('phpcsfixer-level') ,
+          php7ccOpts: preferences.get('php7cc-opts') ,
+          phpmdRulesets: preferences.get('phpmd-rulesets') ,
+          PHPLocation: preferences.get('php-location') ,
+    };
+        setValues(values);
+  }
+  /**
+     * Make sure CS Fixer only has one checkbox checked at any one time.
+     */
+  function fixerCheck() {
+        if ($('#phpcsfixer-level[]').val() === 'all') {
+      $('#phpcsfixer-level[!value="all"]').prop('checked', false);
+        }
+        if ($('#phpcsfixer-level[]').val() === 'psr0') {
+      $('#phpcsfixer-level[!value="prs0"]').prop('checked', false);
+        }
+        if ($('#phpcsfixer-level[]').val() === 'psr1') {
+      $('#phpcsfixer-level[!value="prs1"]').prop('checked', false);
+        }
+        if ($('#phpcsfixer-level[]').val() === 'psr2') {
+      $('#phpcsfixer-level[!value="prs2"]').prop('checked', false);
+        }
+        if ($('#phpcsfixer-level[]').val() === 'symfony') {
+      $('#phpcsfixer-level[!value="symfony"]').prop('checked', false);
+        }
+  }
 
-
-
-
-	/**
-	 * Set each value of the preferences in dialog.
-	 */
-	function setValues( values ) {
-		setCheckboxesFromArray( 'enabled', values.enabledTools );
-		setCheckboxesFromArray( 'phpcs-standards', values.phpcsStandards );
-		setCheckboxesFromArray( 'phpcsfixer-level', values.phpcsfixerLevel );
-		setCheckboxesFromArray( 'php7cc-opts', values.php7ccOpts );
-		setCheckboxesFromArray( 'phpmd-rulesets', values.phpmdRulesets );
-		$dialog.find( 'input[ name="php_location" ]' ).val( values.PHPLocation );
-	}
-	
-	/**
-	 * Check checkboxes according to valuess in array.
-	 */
-	function setCheckboxesFromArray( name, valueArray ) {
-		// Walk through each checkbox in dialog with supplied name.
-		$dialog.find( 'input[ name="' + name + '[]" ]' ).each( function( index, element ) {
-			var $this = $( element );
-			// Make checkbox checked if its value is in array.
-			$this.prop( 'checked', valueArray.indexOf( $this.prop( 'value' ) ) !== -1 );
-		} );
-	}
-	
-	/**
-	 * Initialize dialog values.
-	 */
-	function init() {
-		var values = {
-			enabledTools: preferences.get( 'enabled-tools' ),
-			phpcsStandards: preferences.get( 'phpcs-standards' ),
-			phpcsfixerLevel: preferences.get('phpcsfixer-level'),
-			php7ccOpts: preferences.get('php7cc-opts'),
-			phpmdRulesets: preferences.get( 'phpmd-rulesets' ),
-			PHPLocation: preferences.get( 'php-location' ),
-		};
-		
-		setValues( values );
-	}
-    
-
-	/**
-	 * Make sure CS Fixer only has one checkbox checked at any one time.
-	 */
-
-	function fixerCheck() {
-		if ( $('#phpcsfixer-level[]').val() === 'all') {
-			$('#phpcsfixer-level[!value="all"]').prop('checked', false);
-		} 
-
-		if ( $('#phpcsfixer-level[]').val() === 'psr0') {
-			$('#phpcsfixer-level[!value="prs0"]').prop('checked', false);
-		} 
-
-		if ( $('#phpcsfixer-level[]').val() === 'psr1') {
-			$('#phpcsfixer-level[!value="prs1"]').prop('checked', false);
-		} 
-
-		if ( $('#phpcsfixer-level[]').val() === 'psr2') {
-			$('#phpcsfixer-level[!value="prs2"]').prop('checked', false);
-		}
-
-		if ( $('#phpcsfixer-level[]').val() === 'symfony') {
-			$('#phpcsfixer-level[!value="symfony"]').prop('checked', false);
-		}
-
-	}
-
-	function php7Check() {
-		if ( $('#php7-opts[]').val() === 'error') {
-			$('#php7-opts[!value="error"]').prop('checked', false);
-		} 
-
-		if ( $('#php7-opts[]').val() === 'warning') {
-			$('#php7-opts[!value="warning"]').prop('checked', false);
-		} 
-
-		if ( $('#php7-opts[]').val() === 'info') {
-			$('#php7-opts[!value="info"]').prop('checked', false);
-		} 
-
-	}
-
-
-	/**
-	 * Exposed method to show dialog.
-	 */
-	exports.show = function( prefs ) {
-		// Compile dialog template.
-		var compiledTemplate = Mustache.render( settingsDialogTemplate, {
-			Strings: Strings,
-			phpAvailable: prefs.get( 'php-available' )
-		} );
-		
-		// Save dialog to variable.
-		dialog = Dialogs.showModalDialogUsingTemplate( compiledTemplate );
-		$dialog = dialog.getElement();
-		preferences = prefs;
-		
-		// Initialize dialog values.
-		init();
-		
-		// Register event listeners.
-		$dialog
-			.on( 'click', '.reset-preferences', function() {
-				resetValues();
-			} );
-		
-		// Check PHP Location
-		$dialog.find( '.input-php-location' ).on( 'input', function() {
-			var location = $( this ).val(),
-				okButton = $dialog.find( '[data-button-id="ok"]' ),
-				errorElement = $dialog.find( '.php-location-error' );
-
-			errorElement.hide();
-
-			if ( location ) {
-				okButton.attr( 'disabled', true );
-
-				ParserManager.checkPHPLocation( location, function( phpAvailable ) {
-					if ( phpAvailable ) {
-						okButton.attr( 'disabled', false );
-					} else {
-						errorElement.show();
-					}
-				} );
-			} else {
-				okButton.attr( 'disabled', false );
-			}
-		} );
-
-		$dialog.find( '.input-php-location' ).trigger( 'input' );
-		
-		// Open dialog.
-		dialog.done( function( buttonId ) {
-			var values;
-			
-			// Save preferences if OK button was clicked.
-			if ( buttonId === 'ok' ) {
-				values = getValues();
-				
-				preferences.set( 'enabled-tools', values.enabledTools );
-				preferences.set( 'phpcs-standards', values.phpcsStandards );
-				preferences.set( 'phpcsfixer-level', values.phpcsfixerLevel);
-				preferences.set( 'php7cc-opts', values.php7ccOpts);
-				preferences.set( 'phpmd-rulesets', values.phpmdRulesets );
-				preferences.set( 'php-location', values.PHPLocation );
-				
-				preferences.save();
-				
-				ParserManager.registerEvents();
-			}
-		} );
-	};
-} );
+  function php7Check() {
+        if ($('#php7-opts[]').val() === 'error') {
+      $('#php7-opts[!value="error"]').prop('checked', false);
+        }
+        if ($('#php7-opts[]').val() === 'warning') {
+      $('#php7-opts[!value="warning"]').prop('checked', false);
+        }
+        if ($('#php7-opts[]').val() === 'info') {
+      $('#php7-opts[!value="info"]').prop('checked', false);
+        }
+  }
+  /**
+     * Exposed method to show dialog.
+     */
+  exports.show = function(prefs) {
+        // Compile dialog template.
+        var compiledTemplate = Mustache.render(settingsDialogTemplate, {
+      Strings: Strings,
+          phpAvailable: prefs.get('php-available')
+        });
+        // Save dialog to variable.
+        dialog = Dialogs.showModalDialogUsingTemplate(compiledTemplate);
+        $dialog = dialog.getElement();
+        preferences = prefs;
+        // Initialize dialog values.
+        init();
+        // Register event listeners.
+        $dialog.on('click', '.reset-preferences', function() {
+      resetValues();
+        });
+        // Check PHP Location
+        $dialog.find('.input-php-location').on('input', function() {
+      var location = $(this).val() ,
+          okButton = $dialog.find('[data-button-id="ok"]') ,
+          errorElement = $dialog.find('.php-location-error');
+      errorElement.hide();
+      if (location) {
+        okButton.attr('disabled', true);
+        ParserManager.checkPHPLocation(location, function(phpAvailable) {
+          if (phpAvailable) {
+            okButton.attr('disabled', false);
+          }
+          else {
+            errorElement.show();
+          }
+        });
+      }
+      else {
+        okButton.attr('disabled', false);
+      }
+        });
+        $dialog.find('.input-php-location').trigger('input');
+        // Open dialog.
+        dialog.done(function(buttonId) {
+      var values;
+      // Save preferences if OK button was clicked.
+      if (buttonId === 'ok') {
+                values = getValues();
+                preferences.set('enabled-tools', values.enabledTools);
+                preferences.set('phpcs-standards', values.phpcsStandards);
+                preferences.set('phpcsfixer-level', values.phpcsfixerLevel);
+                preferences.set('php7cc-opts', values.php7ccOpts);
+                preferences.set('phpmd-rulesets', values.phpmdRulesets);
+                preferences.set('php-location', values.PHPLocation);
+                preferences.save();
+                ParserManager.registerEvents();
+      }
+        });
+  };
+});
